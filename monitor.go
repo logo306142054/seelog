e7b1433696380e038c359fecc13be35f27fa9b9e
chenhongxin <306142054@qq.com>
2018-11-29 09:58:44 +0800

    删除不必要的检测文件，TailFile函数会监视文件的创建；TailFile增加参数，自动跳到文件末尾

--- monitor.go
+++ monitor.go
@@ -1,9 +1,9 @@
 package seelog
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/hpcloud/tail"
 )
@@ -12,32 +12,17 @@ import (
 func monitor(filePath string) {
 	defer func() {
 		if err := recover(); err != nil {
-			log.Printf("[seelog] error:%+v", err)
+			log.Printf("[seelog] monitor error:%+v", err)
 		}
 	}()
 
-	//fileInfo := checkFile(filePath)
-	checkFile(filePath)
+	t, _ := tail.TailFile(filePath, tail.Config{Follow: true, ReOpen: true, Location: &tail.SeekInfo{
+		Offset: 0,
+		Whence: os.SEEK_END,
+	}})
 
-	t, _ := tail.TailFile(filePath, tail.Config{Follow: true, ReOpen: true})
 	for line := range t.Lines {
-		//fmt.Println(line.Text)
+		fmt.Println(line.Text)
 		manager.broadcast <- []byte(line.Text)
 	}
-}
-
-//等待文件被创建
-func checkFile(filePath string) os.FileInfo {
-	var fileInfo os.FileInfo
-	var err error
-	for {
-		fileInfo, err = os.Stat(filePath)
-		if err != nil {
-			log.Printf("[seelog] error:%v", err.Error())
-			time.Sleep(10 * time.Millisecond)
-		} else {
-			break
-		}
-	}
-	return fileInfo
 }
